30c60dbeff73a46b4a1a02e5df5f8c97e8773bb2
Martin Drlík <martindrlik@hotmail.com>
2018-12-13 03:20:26 +0100

    new file: perftest/perftest.go

---
+++ perftest/perftest.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var (
+	url        = flag.String("url", "", "")
+	concurrent = flag.Int("concurrent", 10, "")
+	requests   = flag.Int("requests", 10, "")
+)
+
+func main() {
+	checkIsAlive(5) // warmup
+	ch := make(chan int)
+	for i := 0; i < *concurrent; i++ {
+		go func() {
+			ch <- checkIsAlive(*requests)
+		}()
+	}
+	numOK := 0
+	start := time.Now()
+	for i := 0; i < *concurrent; i++ {
+		numOK += <-ch
+	}
+	d := time.Now().Sub(start)
+	total := (*concurrent) * (*requests)
+	fmt.Printf("%d times IsAlive, %d OK, concurrent %d, %v => %v\n",
+		total, numOK, *concurrent, d, float64(total)/d.Seconds())
+}
+
+func checkIsAlive(times int) int {
+	numOK := 0
+	for i := 0; i < times; i++ {
+		res, err := http.Get(*url)
+		if err != nil {
+			log.Fatal()
+		}
+		defer res.Body.Close()
+		if _, err := ioutil.ReadAll(res.Body); err != nil {
+			log.Fatal(err)
+		}
+		if res.StatusCode == http.StatusOK {
+			numOK++
+		}
+	}
+	return numOK
+}
